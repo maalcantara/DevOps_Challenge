96735643792fdccf7a65f51adf6f31cfe58bde68
Mariana Alcantara <marigigialcantara40@gmail.com>
2024-07-22 09:43:02 -0300

    add terraform main

---
+++ terraform/main.tf
@@ -0,0 +1,67 @@
+terraform {
+	backend "azurerm" {
+		resource_group_name = "rg_DesafioDevOps"
+		storage_account_name = "sadesafiodevops"
+		container_name = "conteinerdesafiodevops"
+		key = "terraform.tfstate"
+	}
+
+	required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.99.0"
+    }
+  }
+}
+
+provider "azurerm" { # depois tirar essas credenciais passando pra secret do gh
+  features {
+  }
+}
+
+resource "azurerm_resource_group" "rg_DesafioDevOps" {
+	name = "rg_DesafioDevOps"
+	location = var.resource_location
+}
+
+# virtual network
+resource "azurerm_virtual_network" "res-5" {
+  address_space       = ["10.0.0.0/16"]
+	name                = "vNET_DesafioDevOps"
+  location            = azurerm_resource_group.rg_DesafioDevOps.location
+  resource_group_name = azurerm_resource_group.rg_DesafioDevOps.name
+}
+
+# subnet 1
+resource "azurerm_subnet" "res-6" {
+  address_prefixes     = ["10.0.1.0/26"]
+  name                 = "subnet1_DesafioDevOps"
+  resource_group_name  = azurerm_resource_group.rg_DesafioDevOps.name
+  virtual_network_name = azurerm_virtual_network.res-5.name
+}
+
+# subnet 2
+resource "azurerm_subnet" "res-7" {
+  address_prefixes     = ["10.0.2.0/24"]
+  name                 = "subNET_DesafioDevOps"
+  resource_group_name  = azurerm_resource_group.rg_DesafioDevOps.name
+  virtual_network_name = azurerm_virtual_network.res-5.name
+}
+
+# cluster k8s
+resource "azurerm_kubernetes_cluster" "res-1" {
+  name                      = "cluster_DesafioDevOps"
+	automatic_channel_upgrade = "patch"
+  dns_prefix                = "k8s-devops"
+  location                  = azurerm_resource_group.rg_DesafioDevOps.location
+  resource_group_name       = azurerm_resource_group.rg_DesafioDevOps.name
+  default_node_pool {
+    name    = "agentpool"
+    vm_size = "Standard_DS2_v2"
+    node_count = 2
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
