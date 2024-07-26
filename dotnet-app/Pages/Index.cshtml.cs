ae8427ac92d905d5801aff8192ae46df53215379
Mariana Alcantara <marigigialcantara40@gmail.com>
2024-07-26 10:06:09 -0300

    add views

---
+++ dotnet-app/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnet_webapp.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+        // Method intentionally left empty.
+    }
+}
