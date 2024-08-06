9077ba22898e1590029fd938a99e8c709c8a6cc0
Mariana Alcantara <marigigialcantara40@gmail.com>
2024-08-06 13:47:34 -0300

    remove outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,25 +84,3 @@ resource "azurerm_role_assignment" "aks_acr_pull" {
   role_definition_name      = "AcrPull"
   scope                     = azurerm_container_registry.acrdesafiodevops1.id
 }
-
-# Outputs para usar no pipeline
-output "container_registry_name" {
-  value       = azurerm_container_registry.acrdesafiodevops1.name
-  description = "Container Registry Name"
-}
-
-output "container_registry_login_server" {
-  value       = azurerm_container_registry.acrdesafiodevops1.login_server
-  description = "Container Registry Server to Login"
-}
-
-output "container_registry_admin_username" {
-  value       = azurerm_container_registry.acrdesafiodevops1.admin_username
-  description = "Username for basic authentication to the ACR"
-}
-
-output "container_registry_admin_password" {
-  value       = azurerm_container_registry.acrdesafiodevops1.admin_password
-  description = "Password for basic authentication to the Container Registry"
-  sensitive   = true
-}
