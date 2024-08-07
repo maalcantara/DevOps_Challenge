112d7563cecfa888693dc65a603302f083d7097b
Mariana Alcantara <marigigialcantara40@gmail.com>
2024-08-07 14:23:13 -0300

    add acr resource and its role assignment

--- backend-terraform/main.tf
+++ backend-terraform/main.tf
@@ -30,3 +30,18 @@ resource "azurerm_storage_container" "res-10" {
   storage_account_name  = azurerm_storage_account.res-8.name
   container_access_type = "private"
 }
+
+# ACR e seu Role Assignment
+resource "azurerm_container_registry" "acrdesafiodevops" {
+  name                = "acrdesafiodevops"
+  location            = azurerm_resource_group.rg_DesafioDevOps_backend.location
+  resource_group_name = azurerm_resource_group.rg_DesafioDevOps_backend.name
+  sku                 = "Standard"
+  admin_enabled       = true  # habilitando autenticação básica
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id              = azurerm_kubernetes_cluster.res-1.identity[0].principal_id
+  role_definition_name      = "AcrPull"
+  scope                     = azurerm_container_registry.acrdesafiodevops.id
+}
